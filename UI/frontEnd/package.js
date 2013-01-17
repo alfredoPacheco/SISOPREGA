e405df94244ab7dda29fe40ff8745f3d34a3b83c
alan-delrio <alan.delrio@hotmail.com>
2013-01-16 22:56:27 -0700

    Added logout button and fix bug on login textboxes

---
+++ UI/frontEnd/package.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+enyo.depends( 
+  "$lib/onyx",
+  "$lib/layout",
+  "source/ranchers.js",
+  "source/ranchers.login.js",
+  "source/ranchers.reports.js",  
+  "source/receptions.barnyards.map.js",    
+  "../source/cache.js",
+  "../source/cache.barnyards.js",  
+  "source/cache.receptions.js",    
+  "css/cssx.css",
+  "../css/transitions.css"  
+);
