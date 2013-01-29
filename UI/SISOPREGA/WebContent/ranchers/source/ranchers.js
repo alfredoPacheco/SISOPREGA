4f8c458c5163d98d8fa53882ce6bc38b5a378790
unknown <hpadmin@DELRIOAL1.americas.hpqcorp.net>
2013-01-29 00:23:30 -0700

    moved ranchers website to webproject and added login function to ranchers site

---
+++ UI/SISOPREGA/WebContent/ranchers/source/ranchers.js
@@ -0,0 +1,52 @@
+enyo.kind({
+	name: "ranchers",
+	kind: "FittableRows",
+	classes: "mainBG enyo-fit",	
+	style: "width:100%; height:100%; bottom:0px; top:0px; left:0px;right:0px",
+	fit: true,	
+	components: [
+		{kind: "Panels", name:"main",fit: true,	draggable: false,components:[
+			{kind: "ranchers.login",onSucess:"loadReports"},
+			{kind: "ranchers.reports",onMBYReport:"loadBYMReport", onFeedReport:"loadFeedReport",
+			 onInspectionReport:"loadInspectionReport", onHistoricalReport:"loadHistoricalReport",
+			 onUpdateProfile:"loadUpdateProfile",onGoBack:"loadLogIn"},
+			{kind: "receptions.barnyards.map",onGoBack:"stepBack"}
+		]},
+	],
+	create:function(){
+		this.inherited(arguments);
+		this.$.main.setIndex(0);		
+	},
+	loadLogIn:function(){
+        this.inherited(arguments);
+		this.$.main.setIndex(0);		
+	},	
+	loadUpdateProfile:function(){
+		alert('TODO:Profile')
+	},
+	loadReports:function(){
+        this.inherited(arguments);
+		this.$.main.setIndex(1);		
+	},
+	loadBYMReport:function(){
+        this.inherited(arguments);
+		this.$.main.setIndex(2);		
+	},
+	loadFeedReport:function(){	
+		var sURL="http://localhost:9090/ReportingGateway/RecibidoPorGanadero?fromDate=01/01/2010&toDate=01/01/2014&rancherId=692"
+		window.open(sURL,"reception")
+		//alert('TODO:Feed')
+	},
+	loadInspectionReport:function(){	
+		var sURL="http://localhost:9090/ReportingGateway/CattleInspection?fromDate=01/01/2010&toDate=01/01/2014&rancherId=692"
+		window.open(sURL,"inspection")
+		//alert('TODO:Inspection')	
+	},
+	loadHistoricalReport:function(){
+		http://localhost:9090/ReportingGateway/CattleInspection?fromDate=01/01/2010&toDate=01/01/2014&rancherId=692
+		alert('TODO:Historical')		
+	},		
+	stepBack:function(){
+		this.$.main.setIndex(1);			
+	}
+});
