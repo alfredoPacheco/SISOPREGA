768ad0585431a82919dcbd02e7707d8156646473
alfredoPacheco <j.alfredo.pacheco@gmail.com>
2013-02-01 17:02:16 -0700

    map placed on screen's center

---
+++ UI/SISOPREGA/WebContent/source/inspections.main.fs.js
@@ -0,0 +1,8 @@
+enyo.kind({
+	name: "inspections.main.fs",
+	kind: "Pane",
+	flex: 1,
+	components:[
+		{kind: "inspection.forecast"},			
+	]
+});
--- UI/SISOPREGA/WebContent/source/receptions.barnyards.map.js
+++ UI/SISOPREGA/WebContent/source/receptions.barnyards.map.js
@@ -1,5 +1,5 @@
 enyo.kind({
-	name: "receptions.barnyards.map",
+	name: "receptions.barnyards.map",	
 	arrReception:[{caption:"Recepcion",value:1},{caption:"Deseleccionar",value:2}],
 	arrPostReception:[{caption:"Alimento",value:3},
 				      {caption:"Inspeccion",value:4},
@@ -7,7 +7,8 @@ enyo.kind({
 					  {caption:"Liberar",value:6},
 					  {caption:"Deseleccionar",value:8},
 					  ],
-    kind: enyo.SlidingView,
+    kind: enyo.VFlexBox,
+    flex:1,
 	arrByMOver:{},
 	objSelected:null,
 	arrSelected:{},
@@ -16,21 +17,23 @@ enyo.kind({
 	sColorFree:"white",	
 	sColorSelect:"lightgreen",
 	sColorSelectOccupied:"#9b7eb1",	
+	className:"mapBG",
 	components: [
-		{kind:"VFlexBox", flex:1, className:"mapBG",
-		 components:[
+//		{kind:"VFlexBox", flex:1,className:"mapBG",
+//		 components:[
 			{name: "options", kind: enyo.PopupSelect, onSelect: "actionSelected",items:[]},
-			{kind:enyo.BasicScroller,flex: 1,
+			{kind:enyo.BasicScroller,flex: 1, 
 			components:[
-				{name: "cells", kind: "VFlexBox",align:"middle", onclick: "cellsClick"},
+				{name: "cells", kind: "VFlexBox",align:"center",pack:"center", onclick: "cellsClick"},
 			]},
 			{kind: "Popup",name: "popMan", showHideMode: "transition", openClassName: "zoomFadeIn",
 			 className: "transitioner2", layoutKind: "VFlexLayout",
-			 style: "overflow: hidden", width: "95%", height: "95%",scrim: true,}]}
+			 style: "overflow: hidden", width: "95%", height: "95%",scrim: true,}//]}
 	],
 	ready: function() {
 		this.last=this.$.cells;
 		//this.addRow(true);
+		this.addRow();
 		this.addRowHeader();		
 		this.createCells("2E",7,4,"50px","50px");
 		this.splitRow();
@@ -127,11 +130,12 @@ enyo.kind({
 		objBarn.createComponent({kind:enyo.Control,
 		                         className:sClass,
 								 allowHtml:true,
-		                          style: "width:"+sWidth+
-						                 ";height:"+sHeight+";" +
+		                          style:"width:"+sWidth+
+						                ";height:"+sHeight+";" +
 				                 		"text-align: center;" +
 				                 		"vertical-align: middle;" +
-				                 		"background-color:#DABD8B;",
+				                 		"background-color:#DABD8B;" +
+				                 		"padding-top: 5px;",
 			                      name:sName,
 								  content:sCaption,
 								 },{owner: this});		
@@ -147,9 +151,9 @@ enyo.kind({
 	},
 	addRow:function(bDiv){
 		if (bDiv){	
-			this.$.cells.createComponent({kind: "Divider",caption:"", style:"margin-left: -16px;min-width: 1050px;"});
+			this.$.cells.createComponent({kind: "Divider",caption:"", style:"margin-left:-15px;width: 1040px;"});
 		}else{
-			this.$.cells.createComponent({kind: "HFlexBox", style:"height:5px;"});									
+			this.$.cells.createComponent({kind: "HFlexBox",style:"height:5px;"});									
 		}
 		this.last=objBarn=this.$.cells.createComponent({kind: "HFlexBox"});						
 	},
@@ -157,8 +161,7 @@ enyo.kind({
   		objBarn=this.last;		
 		objBarn.createComponent({kind:enyo.Control,
 		                         style: "width:15px; height:"+sHeight+
-										";align:left"+
-										""});
+										";align:center"});
 	},	
 	createCells:function(sLetter,iStart,iNumber,sWidth,sHeight){
 		//this.createCells("1E",5,6,"50px","50px");
