9aea07a574abeff52a87791528fac908700e561b
alan-delrio <alan.delrio@hotmail.com>
2013-02-05 17:18:28 -0700

    added event on load userList view

--- UI/SISOPREGA/WebContent/source/main.menu.js
+++ UI/SISOPREGA/WebContent/source/main.menu.js
@@ -9,7 +9,7 @@ enyo.kind({
 	},	
 //	     style:"background-image: url(images/practice_background.png); background-size: cover;",				 	
 	components:[
-		{kind: enyo.Pane, className:"buttonsBG", flex: 1, name: "mainPane", transitionKind: "enyo.transitions.LeftRightFlyin", 
+		{kind: enyo.Pane, onSelectView:"selectView",className:"buttonsBG", flex: 1, name: "mainPane", transitionKind: "enyo.transitions.LeftRightFlyin", 
 	     style:"background-size: cover;",				 		
 		 components:[	
 			{kind:"main.menu.options", name:"menuOptions", className:"buttonsBG",style:"width:1030px;margin-left: auto;margin-right: auto;",
@@ -28,7 +28,8 @@ enyo.kind({
 	 		{kind:"receptions.barnyards.map", name:"receptionsMap",lazy:true, flex:1},	 		
 	 		{kind:"reports.main", name:"reports",lazy:true},
 	 		{kind:"inspections.list", name:"inspections"},
-	 		{kind:"inspections.main.fs", name:"inspectionForecast", lazy:true}
+	 		{kind:"inspections.main.fs", name:"inspectionForecast", lazy:true},
+	 		{kind:"users.list", name:"usersList",lazy:true, }	 		
 		 ]}
 	],
 	showReceptionsMap:function(){
@@ -65,13 +66,15 @@ enyo.kind({
       _objMainHeader.setContent('Lista de Usuarios');
       this.addGoBackAction();
       this.$.mainPane.selectViewByName("usersList");
-      if(this.tempFixCom==null){
-    	  this.tempFixCom=this.$.mainPane.createComponent({kind:"users.list", name:"usersList"},{owner: this});    	  
-      }
-      this.$.usersList.updateList();
 	},
 	addGoBackAction:function(){		
 		_gobackStack.push({caption:"Menu Principal",paneMan:this.$.mainPane,paneName:"menuOptions"});		
+	},
+	selectView:function(inSender, inView, inPreviousView) {
+		if(inView.name=="usersList"){
+			inView.updateList();
+		}
 	}
+	
 });
 
