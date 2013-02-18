3d3ee58c6dbb2fa57e52acba45614a35f48d693a
JFigue27 <jaimefigueroa84@gmail.com>
2013-02-17 20:25:16 -0700

    Change code from barnyar.map and color form dates

--- UI/SISOPREGA/WebContent/source/receptions.create.js
+++ UI/SISOPREGA/WebContent/source/receptions.create.js
@@ -33,7 +33,7 @@ enyo.kind({
 			 style: "overflow: hidden", width: "85%", height: "85%",scrim: true, components: [
 				
 			]},
-			{kind: "RowGroup", defaultKind: "HFlexBox", caption: "", style:"color:#FFF",
+			{kind: "RowGroup", defaultKind: "HFlexBox", caption: "", style:"color:black",
 			 components: [
 					  {kind: "Item",
 						components: [
