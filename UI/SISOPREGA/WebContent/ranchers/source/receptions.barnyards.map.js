fb882bb7181eb54a1c8b640f1dc3996d180f46ab
diego-torres <diego.torres.fuerte@gmail.com>
2013-03-03 21:19:46 -0700

    Fixing receptions map on ranchers

--- UI/SISOPREGA/WebContent/ranchers/source/receptions.barnyards.map.js
+++ UI/SISOPREGA/WebContent/ranchers/source/receptions.barnyards.map.js
@@ -92,94 +92,91 @@ enyo.kind(
       this.inherited(arguments);
       this.last = this.$.cells;
       //this.addRow(true);		
-		this.addRowHeader();		
-		this.createCells("2E",-7,4,"50px","50px");
-		this.splitRow();
-		this.addCustomCell("corraman","Corrales de <br/> Manejo","100px","50px");
-		this.createCells("1E",5,6,"50px","50px");
-		this.splitRow();
-		this.createCells("1E",17,8,"50px","50px");
-						
-		this.addRow();		
-		this.createCells("2E",-8,4,"50px","50px");
-		this.splitRow();
-		this.createCells("1E",2,8,"50px","50px");		
-		this.splitRow();
-		this.createCells("1E",18,8,"50px","50px");		
-								
-		this.addRow(true);		
-		this.createCells("2D",-11,1,"50px","50px");
-		this.createCells("2D",-7,2,"50px","50px");
-		this.createCells("2D",-1,1,"50px","50px");
-		this.splitRow();
-		this.createCells("1D",1,8,"50px","50px");				
-		this.splitRow();
-		this.createCells("1D",17,8,"50px","50px");
-								
-		this.addRow();	
-		this.createCells("2D",-12,6,"33.33px","25px");
-		this.splitRow();
-		this.createCells("1D",2,12,"33.33px","25px");
-		this.splitRow();
-		this.createCells("1D",26,4,"100px","25px");		
-		
-		this.addRow(true);	
-		this.createCells("2C",-11,6,"33.33px","25px");
-		this.splitRow();
-		this.createCells("1C",1,12,"33.33px","25px");
-		this.splitRow();
-		this.createCells("1C",25,4,"100px","25px");	
-							
-		this.addRow();
-		this.createCells("2C",-12,6,"33.33px","25px");
-		this.splitRow();
-		this.createCells("1C",2,12,"33.33px","25px");
-		this.splitRow();
-		this.createCells("1C",26,4,"100px","25px");		
-									
-		this.addRow(true);			
-		this.createCells("2B",-11,6,"33.33px","25px");
-		this.splitRow();
-		this.createCells("1B",1,12,"33.33px","25px");
-		this.splitRow();
-		this.createCells("1B",25,4,"100px","25px");						
-									
-		
-		this.addRow(true);
-		this.createCells("2B",-8,4,"50px","50px");
-		this.splitRow();
-		this.createCells("1B",2,8,"50px","50px");
-		this.splitRow();
-		this.createCells("1B",18,6,"50px","50px");		
-		this.addCustomCell("lagoxi","Laguna de <br/> Oxidacion","98px","50px");		
-											
-		this.addRow(true);
-		this.createCells("2A",-7,4,"50px","50px");
-		this.splitRow();
-		this.createCells("1A",1,8,"50px","50px");		
-		this.splitRow();
-		this.createCells("1A",17,8,"50px","50px");		
-											
-		this.addRow();		
-		this.createCells("2A",-8,4,"50px","50px");
-		this.splitRow();
-		this.addCustomCell("cabaA","Caballerizas A","99px","50px");
-		this.addCustomCell("cabaB","Caballerizas B","100px","50px");		
-		this.createCells("1A",10,4,"50px","50px");
-		this.splitRow();
-		this.createCells("1A",18,8,"50px","50px");		
-		
-		
-		this.addRow();	
-		this.createCells("2R",-7,4,"50px","50px");
-		this.splitRow();
-		this.addCustomCell("spacerone","","813px","50px","customBYCellDesc");		
-		
-													
-		this.addRow();
-		this.createCells("2R",-8,4,"50px","50px");
-		this.splitRow();
-		this.addCustomCell("spacertwo","","813px","50px");
+      this.addRowHeader();
+      this.createCells("2E", -7, 4, "50px", "50px");
+      this.splitRow();
+      this.addCustomCell("corraman", "Corrales de <br/> Manejo", "100px", "50px");
+      this.createCells("1E", 5, 6, "50px", "50px");
+      this.splitRow();
+      this.createCells("1E", 17, 8, "50px", "50px");
+
+      this.addRow();
+      this.createCells("2E", -8, 4, "50px", "50px");
+      this.splitRow();
+      this.createCells("1E", 2, 8, "50px", "50px");
+      this.splitRow();
+      this.createCells("1E", 18, 8, "50px", "50px");
+
+      this.addRow(true);
+      this.createCells("2D", -11, 1, "50px", "50px");
+      this.createCells("2D", -7, 2, "50px", "50px");
+      this.createCells("2D", -1, 1, "50px", "50px");
+      this.splitRow();
+      this.createCells("1D", 1, 8, "50px", "50px");
+      this.splitRow();
+      this.createCells("1D", 17, 8, "50px", "50px");
+
+      this.addRow();
+      this.createCells("2D", -12, 6, "33.33px", "25px");
+      this.splitRow();
+      this.createCells("1D", 2, 12, "33.33px", "25px");
+      this.splitRow();
+      this.createCells("1D", 26, 4, "100px", "25px");
+
+      this.addRow(true);
+      this.createCells("2C", -11, 6, "33.33px", "25px");
+      this.splitRow();
+      this.createCells("1C", 1, 12, "33.33px", "25px");
+      this.splitRow();
+      this.createCells("1C", 25, 4, "100px", "25px");
+
+      this.addRow();
+      this.createCells("2C", -12, 6, "33.33px", "25px");
+      this.splitRow();
+      this.createCells("1C", 2, 12, "33.33px", "25px");
+      this.splitRow();
+      this.createCells("1C", 26, 4, "100px", "25px");
+
+      this.addRow(true);
+      this.createCells("2B", -11, 6, "33.33px", "25px");
+      this.splitRow();
+      this.createCells("1B", 1, 12, "33.33px", "25px");
+      this.splitRow();
+      this.createCells("1B", 25, 4, "100px", "25px");
+
+      this.addRow(true);
+      this.createCells("2B", -8, 4, "50px", "50px");
+      this.splitRow();
+      this.createCells("1B", 2, 8, "50px", "50px");
+      this.splitRow();
+      this.createCells("1B", 18, 6, "50px", "50px");
+      this.addCustomCell("lagoxi", "Laguna de <br/> Oxidacion", "98px", "50px");
+
+      this.addRow(true);
+      this.createCells("2A", -7, 4, "50px", "50px");
+      this.splitRow();
+      this.createCells("1A", 1, 8, "50px", "50px");
+      this.splitRow();
+      this.createCells("1A", 17, 8, "50px", "50px");
+
+      this.addRow();
+      this.createCells("2A", -8, 4, "50px", "50px");
+      this.splitRow();
+      this.addCustomCell("cabaA", "Caballerizas A", "99px", "50px");
+      this.addCustomCell("cabaB", "Caballerizas B", "100px", "50px");
+      this.createCells("1A", 10, 4, "50px", "50px");
+      this.splitRow();
+      this.createCells("1A", 18, 8, "50px", "50px");
+
+      this.addRow();
+      this.createCells("2R", -7, 4, "50px", "50px");
+      this.splitRow();
+      this.addCustomCell("spacerone", "", "813px", "50px", "customBYCellDesc");
+
+      this.addRow();
+      this.createCells("2R", -8, 4, "50px", "50px");
+      this.splitRow();
+      this.addCustomCell("spacertwo", "", "813px", "50px");
     },
     last : null,
     addQuitButton : function(sName, sCaption, sWidth, sHeight, sClass) {
@@ -380,9 +377,11 @@ enyo.kind(
       if (objRec.accepted_count != "") {
         objRec.accepted_count;
       }
-      this.$.spacerone.setContent("<strong>" + objRec.rancher_name + "</strong> " + "- " + objRec.city_name + " - " + objRec.cattype_name + "  ("
-          + objRec.weights[0].weight + "/" + objRec.hc_aprox + " KG) Promedio: " + (objRec.weights[0].weight / objRec.hc_aprox) + "KG	 "
-          + objRec.arrival_date + "</br> Corrales (" + iBy + ") - " + sBy);
+      this.$.spacerone.setContent("<strong>" + objRec.rancher_name + "</strong> " + ": " + Math.round(objRec.hc_aprox) + " " + objRec.cattype_name
+          + " de " + objRec.location_name + ". " + objRec.weights[0].weight + " Kg., " + Math.round((objRec.weights[0].weight * 2.2046) * 100) / 100
+          + " Lb. Promedio: " + Math.round((objRec.weights[0].weight / objRec.hc_aprox) * 100) / 100 + "Kg.,"
+          + Math.round((objRec.weights[0].weight / objRec.hc_aprox * 2.2046) * 100) / 100 + "Lb. </br> Corrales ("
+          + iBy + ") - " + sBy);
 
     },
     actionSelected : function(inSender, inSelected) {
