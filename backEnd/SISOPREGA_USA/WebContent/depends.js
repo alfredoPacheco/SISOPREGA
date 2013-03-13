3c759faf19cf78ce9d7e1187f897c3ecf25f48d5
diego-torres <diego.torres.fuerte@gmail.com>
2013-03-12 19:13:34 -0600

    Adding SISOPREGA_USA

---
+++ backEnd/SISOPREGA_USA/WebContent/depends.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+enyo.depends( 
+  "../SISOPREGA/css/transitions.css",
+  "source/sisoprega.us.js",
+  "source/login.js",
+  "source/cache.js"
+);
---
+++ backEnd/SISOPREGA_USA/WebContent/source/login.js
@@ -0,0 +1,134 @@
+enyo.kind(
+  {
+    name : "login",
+    kind : enyo.VFlexBox,
+    events :
+      {
+        "onSucess" : "",
+        "onFail" : "",
+      },
+    components :
+      [
+        {
+          kind : "Toolbar",
+          name : "tbHeader",
+          components :
+            [
+              {
+                kind : "Spacer"
+              },
+              {
+                kind : "VFlexBox",
+                style : "color:#FFF;border:none",
+                content : "SISOPREGA LADO AMERICANO"
+              },
+              {
+                kind : "Spacer"
+              }, ]
+        },
+        {
+          kind : enyo.Scroller,
+          className : "loginBG",
+          flex : 1,
+          components :
+            [
+              {
+                kind : "VFlexBox",
+                align : "center",
+                pack : "center",
+                components :
+                  [
+                    {
+                      kind : "Spacer"
+                    },
+                    {
+                      kind : "Image",
+                      src : "../SISOPREGA/images/eastman.png"
+                    },
+                    {
+                      kind : "RowGroup",
+                      layoutKind : enyo.VFlexLayout,
+                      caption : "",
+                      width : "50%;",
+                      style : "color:#FFF;margin-top:10%",
+                      components :
+                        [
+                          {
+                            kind : "Input",
+                            name : "user",
+                            hint : "Usuario",
+                            selectAllOnFocus : true,
+                            onkeydown : "key_down",
+                            value : "",
+                            onfocus : "on_focus",
+                            onblur : "lost_focus"
+                          },
+                          {
+                            kind : "PasswordInput",
+                            name : "password",
+                            hint : "Contraseña",
+                            selectAllOnFocus : true,
+                            onkeydown : "key_down",
+                            value : "",
+                            onfocus : "on_focus",
+                            onblur : "lost_focus"
+                          } ]
+                    },
+                    {
+                      kind : "Spacer"
+                    } ]
+              } ]
+        },
+        {
+          kind : "Toolbar",
+          name : "tbHeader2",
+          style : "",
+          components :
+            [
+              {
+                kind : "Button",
+                className : "enyo-button-affirmative",
+                style : "background-color:#5f0712",
+                flex : 1,
+                caption : "Entrar",
+                onclick : "checkLogIn",
+                isDefault : "true"
+              }, ]
+        } ],
+    lost_focus : function(InSender, InEvent) {
+      InSender.$.input.applyStyle("color", "white");
+    },
+    on_focus : function(InSender, InEvent) {
+      InSender.$.input.applyStyle("color", "black");
+    },
+    ready : function() {
+      this.$.user.$.input.applyStyle("color", "white");
+      this.$.password.$.input.applyStyle("color", "white");
+      this.$.user.forceFocus();
+    },
+    checkLogIn : function() {
+      cacheMan.showScrim();
+      consumingGateway.Login(this.$.user.getValue(), this.$.password.getValue(), this.loginCallBack, this);
+    },
+    loginCallBack : function(loginResult, objRef) {
+      if (loginResult.exceptionId == 0) {
+        objRef.$.user.setValue("");
+        objRef.$.password.setValue("");
+        // TODO: Evaluate and load based on results
+        cacheMan.hideScrim();
+        objRef.doSucess();
+      } else {
+        cacheMan.hideScrim();
+        objRef.doFail();
+      }
+    },
+    key_down : function(inSender, inEvent) {
+      if (inEvent.keyCode == 13) {
+        if (inSender.name == "user") {
+          this.$.password.forceFocus();
+        } else {
+          this.checkLogIn();
+        }
+      }
+    }
+  });
