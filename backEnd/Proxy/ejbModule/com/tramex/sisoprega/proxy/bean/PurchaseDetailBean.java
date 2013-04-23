58a528e97b2a766858efdf294850a22358b93051
JFigue27 <jaimefigueroa84@gmail.com>
2013-04-22 22:31:10 -0600

    create the proxies for Hermana, Purchase, Expense, and seller

---
+++ backEnd/Proxy/ejbModule/com/tramex/sisoprega/proxy/bean/PurchaseDetailBean.java
@@ -0,0 +1,67 @@
+/**
+ * THIS IS A COMMERCIAL PROGRAM PROVIDED FOR TRAMEX AND IT'S ASSOCIATES
+ * BUILT BY EXTERNAL SOFTWARE PROVIDERS.
+ * THE SOFTWARE COMPRISING THIS SYSTEM IS THE PROPERTY OF TRAMEX OR ITS
+ * LICENSORS.
+ * 
+ * ALL COPYRIGHT, PATENT, TRADE SECRET, AND OTHER INTELLECTUAL PROPERTY RIGHTS
+ * IN THE SOFTWARE COMPRISING THIS SYSTEM ARE, AND SHALL REMAIN, THE VALUABLE
+ * PROPERTY OF TRAMEX OR ITS LICENSORS.
+ * 
+ * USE, DISCLOSURE, OR REPRODUCTION OF THIS SOFTWARE IS STRICTLY PROHIBITED,
+ * EXCEPT UNDER WRITTEN LICENSE FROM TRAMEX OR ITS LICENSORS.
+ * 
+ * &copy; COPYRIGHT 2012 TRAMEX. ALL RIGHTS RESERVED.
+ */
+package com.tramex.sisoprega.proxy.bean;
+
+import com.tramex.sisoprega.common.BaseResponse;
+import com.tramex.sisoprega.common.CreateGatewayResponse;
+import com.tramex.sisoprega.common.GatewayRequest;
+import com.tramex.sisoprega.common.ReadGatewayResponse;
+import com.tramex.sisoprega.common.UpdateGatewayResponse;
+import com.tramex.sisoprega.common.crud.Cruddable;
+
+/**
+ * @author Jaime Figueroa
+ *
+ */
+public class PurchaseDetailBean implements Cruddable {
+
+  /* (non-Javadoc)
+   * @see com.tramex.sisoprega.common.crud.Cruddable#Create(com.tramex.sisoprega.common.GatewayRequest)
+   */
+  @Override
+  public CreateGatewayResponse Create(GatewayRequest request) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  /* (non-Javadoc)
+   * @see com.tramex.sisoprega.common.crud.Cruddable#Read(com.tramex.sisoprega.common.GatewayRequest)
+   */
+  @Override
+  public ReadGatewayResponse Read(GatewayRequest request) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  /* (non-Javadoc)
+   * @see com.tramex.sisoprega.common.crud.Cruddable#Update(com.tramex.sisoprega.common.GatewayRequest)
+   */
+  @Override
+  public UpdateGatewayResponse Update(GatewayRequest request) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  /* (non-Javadoc)
+   * @see com.tramex.sisoprega.common.crud.Cruddable#Delete(com.tramex.sisoprega.common.GatewayRequest)
+   */
+  @Override
+  public BaseResponse Delete(GatewayRequest request) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
