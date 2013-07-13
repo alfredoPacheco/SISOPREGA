89a3e2b158eace9bb2a85ebf25ebd4f4125be7da
diego-torres <diego.torres.fuerte@gmail.com>
2013-07-12 22:28:26 -0600

    Adding inspection for released reception

--- backEnd/BusinessLogicProxy/ejbModule/com/tramex/sisoprega/proxy/bean/ReceptionBean.java
+++ backEnd/BusinessLogicProxy/ejbModule/com/tramex/sisoprega/proxy/bean/ReceptionBean.java
@@ -11,6 +11,7 @@ import java.util.Set;
 import javax.annotation.security.RolesAllowed;
 import javax.ejb.Stateless;
 
+import com.tramex.sisoprega.dto.Inspection;
 import com.tramex.sisoprega.dto.InspectionForecast;
 import com.tramex.sisoprega.dto.InspectionForecastDetail;
 import com.tramex.sisoprega.dto.Pen;
@@ -39,8 +40,17 @@ public class ReceptionBean extends BaseBean implements Cruddable {
 
       GatewayRecord record = request.getParentRecord();
       Class<?> type = Class.forName(DTO_PACKAGE + record.getEntity());
-      Object entity = getEntityFromRecord(record, type);
-      this.log.fine("Found entity from Record [" + entity + "]");
+      Reception entity = (Reception) getEntityFromRecord(record, type);
+      this.log.fine("Found reception from Record [" + entity + "]");
+
+      if (!hasPen(entity)) {
+        error_description = "El registro de recepcion que esta intentando agregar no ha requerido el uso de ningún corral. "
+            + "Toda recepción debe estar relacionada a por lo menos un corral del mapa de operaciones.";
+
+        this.log.warning("Error de validación: " + error_description);
+        response.setError(new GatewayError("VAL01", "Error de validación: " + error_description, "Create"));
+        return response;
+      }
 
       if (this.validateEntity(entity)) {
         this.log.fine("Valid entity found");
@@ -48,8 +58,7 @@ public class ReceptionBean extends BaseBean implements Cruddable {
         dataModel.createDataModel(entity);
 
         log.fine("Reception record created, setting InspectionForecast");
-        Reception reception = (Reception) entity;
-        setInspectionForecast(reception);
+        setInspectionForecast(entity);
 
         List<Object> updatedRecordList = new ArrayList<Object>();
         log.fine("Setting as response object: {" + entity + "}");
@@ -89,7 +98,7 @@ public class ReceptionBean extends BaseBean implements Cruddable {
     try {
       GatewayRecord record = request.getParentRecord();
       Class<?> type = Class.forName(DTO_PACKAGE + record.getEntity());
-      Object entity = getEntityFromRecord(record, type);
+      Reception entity = (Reception) getEntityFromRecord(record, type);
       this.log.fine("Found entity from Record [" + entity + "]");
 
       if (this.validateEntity(entity)) {
@@ -108,16 +117,29 @@ public class ReceptionBean extends BaseBean implements Cruddable {
           response.setError(new GatewayError("VAL04", "Se ha omitido el id en la entidad [" + record.getEntity()
               + "] al intentar actualizar sus datos.", "Update"));
         } else {
+          
+          if(!hasPen(entity)){
+            // Create inspection record if it does not exists
+            if(entity.getInspection() == null || entity.getInspection().size()==0){
+              log.info("Adding inspection to released reception");
+              Inspection inspection = new Inspection();
+              inspection.setInspectionDate(new Date());
+              inspection.setComments("Ganado liberado sin defectos");
+              entity.addInspection(inspection);
+            }
+          }
+          
           dataModel.updateDataModel(entity);
 
           String queryName = record.getEntity().toUpperCase() + "_BY_ID";
           log.fine("Retrieving updated object with Query: " + queryName);
 
-          entity = dataModel.readSingleDataModel(queryName, "Id", idToUpdate, type);
+          entity = (Reception) dataModel.readSingleDataModel(queryName, "Id", idToUpdate, type);
           log.fine("got updated record: [" + entity + "], setting inspection forecast");
 
-          Reception reception = (Reception) entity;
-          setInspectionForecast(reception);
+          
+          if(getTomorrow(entity.getDateAllotted()).compareTo(getTomorrow(new Date())) >= 0 )
+            setInspectionForecast(entity);
 
           List<Object> updatedRecordList = new ArrayList<Object>();
           updatedRecordList.add(entity);
@@ -152,35 +174,28 @@ public class ReceptionBean extends BaseBean implements Cruddable {
     this.log.entering(this.getClass().getCanonicalName(), "void setInspectionForecast(Reception)");
     InspectionForecast ifc = getInspectionForecast(reception.getDateAllotted());
     log.fine("got InspectionForecast:[" + ifc + "]");
-    InspectionForecastDetail ifd = getInspectionForecastDetail(ifc, reception);
-    log.fine("got InspectionForecastDetail:[" + ifd + "]");
+    
+    if(!ifc.isLocked()){
+      InspectionForecastDetail ifd = getInspectionForecastDetail(ifc, reception);
+      log.fine("got InspectionForecastDetail:[" + ifd + "]");
 
-    if (ifd.getInspectionForecastDetailId() == 0)
-      ifc.addInspectionForecastDetail(ifd);
+      if (ifd.getInspectionForecastDetailId() == 0)
+        ifc.addInspectionForecastDetail(ifd);
 
-    if (ifc.getInspectionForecastId() == 0)
-      dataModel.createDataModel(ifc);
-    else
-      dataModel.updateDataModel(ifc);
+      if (ifc.getInspectionForecastId() == 0){
+        log.fine("inspection forecast not found in database, creating one: " + ifc);
+        dataModel.createDataModel(ifc);
+      }
+      
+    }else{
+      log.fine("Inspection forecast is already locked");
+    }
 
     this.log.exiting(this.getClass().getCanonicalName(), "void setInspectionForecast(Reception)");
   }
 
-  @Override
-  protected boolean validateEntity(Object entity) {
-    Reception reception = null;
-    if (entity instanceof Reception)
-      reception = (Reception) entity;
-    else
-      return super.validateEntity(entity);
-
-    if (reception.getPen() == null || reception.getPen().size() == 0) {
-      error_description = "El registro de recepcion que esta intentando agregar no ha requerido el uso de ningún corral. "
-          + "Toda recepción debe estar relacionada a por lo menos un corral del mapa de operaciones.";
-      return false;
-    }
-
-    return true;
+  private boolean hasPen(Reception reception) {
+    return !(reception.getPen() == null || reception.getPen().size() == 0);
   }
 
   private InspectionForecastDetail getInspectionForecastDetail(InspectionForecast forecast, Reception reception) {
@@ -222,25 +237,31 @@ public class ReceptionBean extends BaseBean implements Cruddable {
   private InspectionForecast getInspectionForecast(Date inspectionDate) {
 
     InspectionForecast ifc = null;
-
-    // calculate tomorrow of reception date alloted.
-    Calendar cal1 = Calendar.getInstance();
-    cal1.setTime(inspectionDate);
-    cal1.add(Calendar.DATE, 1); // For tomorrow
+    Date forecastDate = getTomorrow(inspectionDate);
 
     Map<String, Object> parameters = new HashMap<String, Object>();
-    parameters.put("forecastDate", cal1.getTime());
+    parameters.put("forecastDate", forecastDate);
 
     List<InspectionForecast> ifcList = dataModel.readDataModelList("INSPECTIONFORECAST_BY_DATE", parameters,
         InspectionForecast.class);
 
     if (ifcList == null || ifcList.isEmpty()) {
       ifc = new InspectionForecast();
-      ifc.setForecastDate(cal1.getTime());
+      ifc.setForecastDate(forecastDate);
     } else {
       ifc = ifcList.get(0);
     }
 
     return ifc;
+  }
+  
+  private Date getTomorrow(Date date){
+    // calculate tomorrow of given date.
+    Calendar cal = Calendar.getInstance();
+    cal.setTime(date);
+    cal.add(Calendar.DATE, 1); // For tomorrow
+    
+    return cal.getTime();
+    
   }
 }
