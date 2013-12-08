93f9f80f4b8ada16dea557c1b9b8574ad6f52ddf
diego-torres <diego.torres.fuerte@gmail.com>
2013-12-07 18:58:41 -0700

    Fixing issuel #119: complete pen names for moving cattle

--- UI/SISOPREGA_USA/WebContent/source/operations/movePen.js
+++ UI/SISOPREGA_USA/WebContent/source/operations/movePen.js
@@ -1,88 +1,105 @@
 enyo
-	.kind({
-	    name : "movePen",
-	    kind : enyo.VFlexBox,
-	    objFrom : {},
-	    objTo : {},
-	    events : {
+	.kind(
+	{
+	  name : "movePen",
+	  kind : enyo.VFlexBox,
+	  objFrom : {},
+	  objTo : {},
+	  events :
+	  {
 		onCancel : "",
 		onGuardar : ""
-	    },
-	    style : "padding:10px;font-size:17px;background-color:#DABD8B;",
-	    components : [ {
+	  },
+	  style : "padding:10px;font-size:17px;background-color:#DABD8B;",
+	  components : [
+	  {
 		kind : enyo.HFlexBox,
 		align : "center",
 		pack : "center",
 		height : "40px;",
-		components : [ {
-		    content : "Moving from:",
-		    width : "200px;",
-		    style : "text-align: right;margin-right:10px;"
-		}, {
-		    name : 'lblPen',
-		    style : "color:black",
-		    width : "80px",
-		    content : ""
-		}, {
-		    content : "to:",
-		    width : "13px;",
-		    style : "text-align: right;margin-right:10px;"
-		}, {
-		    name : 'lblPenTo',
-		    style : "color:black",
-		    width : "80px",
-		    content : ""
-		}, {
-		    kind : "enyo.Spacer",
-		    flex : 1
+		components : [
+		{
+		  content : "Moving from:",
+		  width : "200px;",
+		  style : "text-align: right;margin-right:10px;"
+		},
+		{
+		  name : 'lblPen',
+		  style : "color:black",
+		  width : "80px",
+		  content : ""
+		},
+		{
+		  content : "to:",
+		  width : "13px;",
+		  style : "text-align: right;margin-right:10px;"
+		},
+		{
+		  name : 'lblPenTo',
+		  style : "color:black",
+		  width : "80px",
+		  content : ""
+		},
+		{
+		  kind : "enyo.Spacer",
+		  flex : 1
 		} ]
-	    }, {
+	  },
+	  {
 		kind : enyo.HFlexBox,
 		align : "center",
 		pack : "center",
 		height : "40px;",
-		components : [ {
-		    content : "Heads on Pen:",
-		    width : "200px;",
-		    style : "text-align: right;margin-right:10px;"
-		}, {
-		    name : 'lblHeadsInPen',
-		    style : "color:black",
-		    flex : 1,
-		    content : ""
+		components : [
+		{
+		  content : "Heads on Pen:",
+		  width : "200px;",
+		  style : "text-align: right;margin-right:10px;"
+		},
+		{
+		  name : 'lblHeadsInPen',
+		  style : "color:black",
+		  flex : 1,
+		  content : ""
 		}, ]
-	    }, {
+	  },
+	  {
 		kind : enyo.HFlexBox,
 		align : "center",
 		pack : "center",
 		height : "40px;",
-		components : [ {
-		    content : "Available to move",
-		    width : "200px;",
-		    style : "text-align: right;margin-right:10px;"
-		}, {
-		    name : 'lblHeadsMobile',
-		    style : "color:black",
-		    flex : 1,
-		    content : ""
+		components : [
+		{
+		  content : "Available to move",
+		  width : "200px;",
+		  style : "text-align: right;margin-right:10px;"
+		},
+		{
+		  name : 'lblHeadsMobile',
+		  style : "color:black",
+		  flex : 1,
+		  content : ""
 		}, ]
-	    }, {
+	  },
+	  {
 		kind : enyo.HFlexBox,
 		align : "center",
 		pack : "center",
 		height : "40px;",
-		components : [ {
-		    content : "Date/Time:",
-		    width : "200px;",
-		    style : "text-align: right;margin-right:10px;"
-		}, {
-		    kind : "ToolInput",
-		    name : "moveDate",
-		    hint : "mm/dd/yyyy",
-		    // width : "103px;",
-		    flex : 1,
-		    height : "35px;",
-		    onfocus : "applyMask",
+		components : [
+		{
+		  content : "Date/Time:",
+		  width : "200px;",
+		  style : "text-align: right;margin-right:10px;"
+		},
+		{
+		  kind : "ToolInput",
+		  name : "moveDate",
+		  hint : "mm/dd/yyyy",
+		  // width : "103px;",
+		  flex : 1,
+		  height : "35px;",
+		  onfocus : "applyMask",
 		// style:"text-align: right;max-width: 500px;"
 		},
 		// {
@@ -93,110 +110,120 @@ enyo
 		// width : "80px;"
 		// },
 		{
-		    kind : "ToolInput",
-		    name : "moveTime",
-		    // width : "103px;",
-		    hint : "HH:MM",
-		    flex : 1,
-		    height : "35px;",
-		    onfocus : "applyTimeMask",
+		  kind : "ToolInput",
+		  name : "moveTime",
+		  // width : "103px;",
+		  hint : "HH:MM",
+		  flex : 1,
+		  height : "35px;",
+		  onfocus : "applyTimeMask",
 		// style:"text-align: right;max-width: 500px;"
 		} ]
-	    }, {
+	  },
+	  {
 		kind : enyo.HFlexBox,
 		align : "center",
 		pack : "center",
 		height : "40px;",
-		components : [ {
-		    content : "Heads to Move:",
-		    width : "200px;",
-		    style : "text-align: right;margin-right:10px;"
-		}, {
-		    kind : "controls.numberBox",
-		    inputKind:"ToolInput",
-		    height:"35px",
-		    name : "totalHC",
-		    hint : '',
-		    flex : 1,
+		components : [
+		{
+		  content : "Heads to Move:",
+		  width : "200px;",
+		  style : "text-align: right;margin-right:10px;"
+		},
+		{
+		  kind : "controls.numberBox",
+		  inputKind : "ToolInput",
+		  height : "35px",
+		  name : "totalHC",
+		  hint : '',
+		  flex : 1,
 		// style:"max-width: 500px;"
 		}, ]
-	    }, {
+	  },
+	  {
 		kind : enyo.HFlexBox,
 		align : "center",
 		height : "40px;",
 		style : "font-size:14px;",
-		components : [ {
-		    kind : enyo.Spacer
-		}, {
-		    kind : enyo.Button,
-		    caption : "Save",
-		    onclick : "save",
-		    style : "background-color: #DABD8B;min-width:70px;"
-		}, {
-		    kind : enyo.Button,
-		    caption : "Cancel",
-		    onclick : "doCancel",
-		    style : "background-color: #DABD8B;min-width:70px;"
+		components : [
+		{
+		  kind : enyo.Spacer
+		},
+		{
+		  kind : enyo.Button,
+		  caption : "Save",
+		  onclick : "save",
+		  style : "background-color: #DABD8B;min-width:70px;"
+		},
+		{
+		  kind : enyo.Button,
+		  caption : "Cancel",
+		  onclick : "doCancel",
+		  style : "background-color: #DABD8B;min-width:70px;"
 		}, ]
-	    }
-	    // ]},
-	    ],
-	    ready : function() {
+	  }
+	  // ]},
+	  ],
+	  ready : function() {
 		this.$.moveDate.setValue(utils.dateOut(new Date()));
 		this.$.moveDate.$.input.applyStyle("text-align", "left");
 		this.$.moveTime.setValue(new Date().toLocaleTimeString()
 			.substring(0, 5));
-	    },
-	    applyMask : function(inSender) {
+	  },
+	  applyMask : function(inSender) {
 		var _id = inSender.$.input.getId();
 		jQuery(function(j) {
-		    j(document.getElementById(_id)).mask('99/99/9999');
+		  j(document.getElementById(_id)).mask('99/99/9999');
 		});
-	    },
-	    applyTimeMask : function(inSender) {
+	  },
+	  applyTimeMask : function(inSender) {
 		var _id = inSender.$.input.getId();
 		jQuery(function(j) {
-		    j(document.getElementById(_id)).mask('99:99');
+		  j(document.getElementById(_id)).mask('99:99');
 		});
-	    },
-	    setObj : function(objFrom, objTo) {
+	  },
+	  setObj : function(objFrom, objTo) {
 		this.objFrom = objFrom;
 		this.objTo = objTo;
-		this.$.lblPen
-			.setContent(crudPen.getByID(objFrom.penId).barnyardCode);
-		this.$.lblPenTo
-			.setContent(crudPen.getByID(objTo.penId).barnyardCode);
-		this.$.lblHeadsInPen.setContent(objFrom.heads);		
+		
+		var penFrom = crudPen.adapterToList(crudPen.getByID(objFrom.penId)).caption;
+		var penTo = crudPen.adapterToList(crudPen.getByID(objTo.penId)).caption;
+		this.$.lblPen.setContent(penFrom);
+		this.$.lblPenTo.setContent(penTo);
+		
+		this.$.lblHeadsInPen.setContent(objFrom.heads);
 		this.$.lblHeadsMobile.setContent(objFrom.availableToSell);
 		this.$.totalHC.setValue("0");
-	    },
-	    getObj : function() {
+	  },
+	  getObj : function() {
 		return this.objFrom;
-	    },
-	    validateForm : function() {
+	  },
+	  validateForm : function() {
 		var sError = "";
 		var headsToMove = Number(this.$.totalHC.getValue());
 		var availableToMove = Number(this.$.lblHeadsMobile.getContent());
-		if (!headsToMove || headsToMove < 1 || headsToMove == '' || headsToMove <= 0) {
-		    sError = "Verify the heads quantity.";
+		if (!headsToMove || headsToMove < 1 || headsToMove == ''
+			|| headsToMove <= 0) {
+		  sError = "Verify the heads quantity.";
 		}
 		if (headsToMove > availableToMove) {
-		    sError = "The amount of heads you want to move can't be greater than available heads.";
+		  sError = "The amount of heads you want to move can't be greater than available heads.";
 		}
-		
+
 		if (sError != "") {
-		    cacheMan.setMessage("", sError);
-		    return false;
+		  cacheMan.setMessage("", sError);
+		  return false;
 		}
 		return true;
-	    },
-	    save : function() {
+	  },
+	  save : function() {
 		if (this.validateForm()) {
-		    var aux = parseFloat(this.objFrom.weight)
-			    / parseInt(this.objFrom.heads);
-		    this.objFrom.heads = parseInt(this.$.totalHC.getValue());
-		    this.objFrom.weight = aux * this.objFrom.heads;
-		    this.doGuardar();
+		  var aux = parseFloat(this.objFrom.weight)
+			  / parseInt(this.objFrom.heads);
+		  this.objFrom.heads = parseInt(this.$.totalHC.getValue());
+		  this.objFrom.weight = aux * this.objFrom.heads;
+		  this.doGuardar();
 		}
-	    }
+	  }
 	});
