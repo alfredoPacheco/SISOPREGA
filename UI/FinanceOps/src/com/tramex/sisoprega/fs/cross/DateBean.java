bdb8f7e0f09d870a435b565574b0975d5ad59ce8
diego-torres <diego.torres.fuerte@gmail.com>
2014-01-02 17:41:26 -0700

    Prices model completed, working on calendar

---
+++ UI/FinanceOps/src/com/tramex/sisoprega/fs/cross/DateBean.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package com.tramex.sisoprega.fs.cross;
+
+import java.util.Date;
+
+/**
+ * USAGE COMMENT HERE
+ *  
+ * <B>Revision History:</B>
+ * 
+ * <PRE>
+ * ====================================================================================
+ * Date        By                           Description
+ * MM/DD/YYYY
+ * ----------  ---------------------------  -------------------------------------------
+ * Jan 2, 2014     Diego Torres                 Initial Version.
+ * ====================================================================================
+ * </PRE>
+ * 
+ * @author Diego Torres
+ *
+ * 
+ */
+public class DateBean {
+  private Date calendarDate;
+
+  /**
+   * @return the date
+   */
+  public Date getCalendarDate() {
+    return calendarDate;
+  }
+
+  /**
+   * @param date the date to set
+   */
+  public void setCalendarDate(Date calendarDate) {
+    this.calendarDate = calendarDate;
+  }
+  
+  
+}
